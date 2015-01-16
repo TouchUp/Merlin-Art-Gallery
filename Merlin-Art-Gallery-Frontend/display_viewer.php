8c120a0413d3f3e7fdbebef05cb09df7e38a1515
lieuzhenghong <lieuzhenghong@gmail.com>
2015-01-17 00:49:31 +0800

    Did some code on display_viewer.php

--- Merlin-Art-Gallery-Frontend/display_viewer.php
+++ Merlin-Art-Gallery-Frontend/display_viewer.php
@@ -14,12 +14,11 @@
 
 		function nextPicture (){
 			//this is the actual function that transitions
-
 			//display the next image in the array
 			i++;
 			tempImage == imageArray[i];
 			document.getElementById('picture').innerHTML =  '<img src = "' + tempImage + '">';
-
+			displayInfo(); //calls the function to display related info as well
 		}
 
 		function displayOrNot(){
@@ -29,12 +28,48 @@
 			var boolArray = [];
 			boolArray["id"] = false;
 			boolArray["painting_name"] = false;
-			//......
+			boolArray["artist_name"] = false;
+			boolArray["price"]=false;
+			boolArray["cm_height"] = false;
+			boolArray["cm_width"] = false;
+			boolArray["in_height"] = false;
+			boolArray["in_width"] = false;
+			boolArray["biography"] = false;
+			boolArray["other"] = false;
 
-			
+			//This checks which checkboxes the user has checked
+			boolArray["id"] = <?php isset($_POST)['id'];?>;
+			boolArray["painting_name"] = <?php isset($_POST)['painting_name'];?>;
+			boolArray["artist_name"] = <?php isset($_POST)['artist_name'];?>;
+			boolArray["price"] = <?php isset($_POST)['price'];?>;
+			boolArray["cm_height"] = <?php isset($_POST)['cm_height'];?>;
+			boolArray["cm_width"] = <?php isset($_POST)['cm_width'];?>;
+			boolArray["in_height"] = <?php isset($_POST)['in_height'];?>;
+			boolArray["in_width"] = <?php isset($_POST)['in_width'];?>;
+			boolArray["biography"] = <?php isset($_POST)['biography'];?>;
+			boolArray["other"] = <?php isset($_POST)['other'];?>;
 
+			}	
 
-		}
+		function displayInfo(){
+
+			var imageInfo = [];
+			var id = <?php $id; ?>; //I don't know how to get the primary key using php
+
+			/* This is pseudocode 
+			for (each element in boolArray){
+				get the information of that primary key's element
+				imageInfo.push;
+			}
+			*/
+
+			//concatenate all the innerHTML; loops through all
+			for (i=0; i <= imageInfo.length; i ++) {
+				var temp = imageInfo[i].toString();
+				document.getElementById('description').innerHTML += "<br>" + temp;			
+			}
+	}
+
 
 	</script>
 
