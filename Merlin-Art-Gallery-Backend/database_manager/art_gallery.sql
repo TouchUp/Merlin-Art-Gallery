cc708b6f9a57ae4a8f77c6f959d9b4e3b2656c93
lieuzhenghong <lieuzhenghong@gmail.com>
2015-01-17 14:29:23 +0800

    Fix to art_gallery.sql
    seriously dae koon stop fucking importing directly from phpMyAdmin

--- Merlin-Art-Gallery-Backend/database_manager/art_gallery.sql
+++ Merlin-Art-Gallery-Backend/database_manager/art_gallery.sql
@@ -7,7 +7,6 @@
 -- Server version: 5.6.21
 -- PHP Version: 5.6.3
 
-CREATE DATABASE IF NOT EXISTS imageserver
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -21,6 +20,9 @@ SET time_zone = "+00:00";
 --
 -- Database: `imageserver`
 --
+
+CREATE DATABASE IF NOT EXISTS `imageserver`;
+USE `imageserver`;
 
 -- --------------------------------------------------------
 
