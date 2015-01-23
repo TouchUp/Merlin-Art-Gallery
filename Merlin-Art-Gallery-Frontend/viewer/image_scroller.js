9af8026d50dc1780fecb49a6be4b364682a7be52
lieuzhenghong <lieuzhenghong@Simonss-MacBook-Pro.local>
2015-01-23 10:06:02 +0800

    Did a tentative version of image_scroller.js

--- Merlin-Art-Gallery-Frontend/viewer/image_scroller.js
+++ Merlin-Art-Gallery-Frontend/viewer/image_scroller.js
@@ -1,7 +1,10 @@
+var zoom = false;
+//Not used 
+
 // Get dimensions of the viewport
 
-var viewport_w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0)
-var viewport_h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0)
+var viewport_w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+var viewport_h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
 
 //Get dimensions of the current image
 
@@ -12,7 +15,7 @@ var current_h = img.clientHeight;
 
 //Get dimensions of the black bar
 
-var info_bar = document.getElementById('description')
+var info_bar = document.getElementById('description');
 
 var bar_h =  info_bar.clientHeight;
 var bar_w = info_bar.clientWidth;
@@ -26,8 +29,8 @@ var usable_h = viewport_h - bar_h;
 
 function sizeChecker() {
 	//This function checks the size of the image
-	if ((current_w < usable_w || current_h < usable_h) && zoom == true){
-		zoomImage();		
+	if ((current_w < usable_w || current_h < usable_h) && zoom === true){
+		zoomImage();
 	}
 	else if (current_w < usable_w  && current_h < usable_h){
 			scrollImage_x();
@@ -54,9 +57,12 @@ function scrollImage_x() {
 	//Let's first find by how much the image overflows.
 	var overflow_w = (current_w - usable_w);
 	//Now, we translate in the positive-x axis by this overflow amount
+	document.getElementById('picture').style.marginLeft = overflow_w;
 
 }
 
 function scrollImage_y() {
+	var overflow_h = (current_h - usable_h);
+	document.getElementById('picture').style.marginTop = overflow_h;
 
 }
