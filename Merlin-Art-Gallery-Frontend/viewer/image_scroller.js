632c675be4a4547b2b28d42fa77542464ba3c8cb
lieuzhenghong <lieuzhenghong@Simonss-MacBook-Pro.local>
2015-01-29 11:16:38 +0800

    tentative working image_scroller.js

--- Merlin-Art-Gallery-Frontend/viewer/image_scroller.js
+++ Merlin-Art-Gallery-Frontend/viewer/image_scroller.js
@@ -1,49 +1,55 @@
-var zoom = false;
+var ZOOM = false;
 //Not used 
 
 // Get dimensions of the viewport
 
-var viewport_w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-var viewport_h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+var VIEWPORT_W = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+var VIEWPORT_H = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
 
 //Get dimensions of the current image
 
-var img =  document.getElementById('picture'); //doesn't work; returns null
-alert(img);
+var IMG =  document.getElementById('picture'); //doesn't work; returns null
+alert(IMG);
 
-var current_w = img.clientWidth;
-var current_h = img.clientHeight;
+var CURRENT_W = IMG.clientWidth;
+var CURRENT_H = IMG.clientHeight;
 
 //Get dimensions of the black bar
 
-var info_bar = document.getElementById('description'); //doesn't work; returns null
+var INFO_BAR = document.getElementById('description'); //doesn't work; returns null
 
-var bar_h =  info_bar.clientHeight;
-var bar_w = info_bar.clientWidth;
+var BAR_H =  INFO_BAR.clientHeight;
+var BAR_W = INFO_BAR.clientWidth;
 
-alert(bar_h);
+alert(BAR_H);
 
 // Get dimensions of the actual, usable area 
 
-var usable_w = viewport_w - bar_w;
-var usable_h = viewport_h - bar_h;
+var USABLE_W = VIEWPORT_W - BAR_W;
+var USABLE_H = VIEWPORT_H - BAR_H;
 
-//Alternative functionality to zoom in and out the image
+//Scrolling speed of image 
+//Doesn't work yet
+
+var SCROLL_SPEED = 0;
+
+// This calls the div id 'picture'
+PICTURE = document.getElementById('picture');
 
 function sizeChecker() {
 	//This function checks the size of the image
-	if ((current_w < usable_w || current_h < usable_h) && zoom === true){
+	if ((CURRENT_W < USABLE_W || CURRENT_H < USABLE_H) && ZOOM === true){
 		zoomImage();
 	}
-	else if (current_w < usable_w  && current_h < usable_h){
+	else if (CURRENT_W < USABLE_W  && CURRENT_H < USABLE_H){
 			scrollImage_x();
 			scrollImage_y();
 			//This will scroll both simultaneously, causing diagonal scrolling (unwanted behaviour!!)			
 		}
-	else if (current_w < usable_w) {
+	else if (CURRENT_W < USABLE_W) {
 		scrollImage_x();
 	}
-	else if (current_h < usable_h) {
+	else if (CURRENT_H < USABLE_H) {
 		scrollImage_y();
 	}
 }
@@ -52,20 +58,36 @@ function zoomImage() {
 	/* How much to zoom? The following piece of code 
 	obtains the ratio of container to image, then scales it appropriately 
 	by that amount */
-	var toZoom_x = (usable_w / current_w);
-	var toZoom_y = (usable_h / current_h);
+	var toZoom_x = (USABLE_W / CURRENT_W);
+	var toZoom_y = (USABLE_H / CURRENT_H);
 }
 
 function scrollImage_x() {
 	//Let's first find by how much the image overflows.
-	var overflow_w = (current_w - usable_w);
+	var overflow_w = (CURRENT_W - USABLE_W);
 	//Now, we translate in the positive-x axis by this overflow amount
-	document.getElementById('picture').style.marginLeft = overflow_w;
+
+	PICTURE.style.marginLeft = +overflow_w+ 'px';
+	alert('Margin left:' + PICTURE.style.marginLeft);
+	//Then we get the scrolling speed
+	//document.getElementById('picture.translate_x').style.-webkit-transition = +scrollSpeed+ 's';
+	//document.getElementById('picture.translate_x').style.-moz-transition = +scrollSpeed+ 's';
+	//document.getElementById('picture.translate_x').style.transition = +scrollSpeed+ 's';
+
+	//Then, let's add the css class to the div such that it starts animating
+	picture.style.marginLeft.classList.add('translate_x');
 
 }
 
 function scrollImage_y() {
-	var overflow_h = (current_h - usable_h);
-	document.getElementById('picture').style.marginTop = overflow_h;
+	var overflow_h = (CURRENT_H - USABLE_H);
+
+	PICTURE.style.marginTop = +overflow_h+ 'px'; 
+	alert('Margin top:' + PICTURE.style.marginTop);
+	//document.getElementById('picture.translate_y').style.-webkit-transition = +scrollSpeed+ 's';
+	//document.getElementById('picture.translate_y').style.-moz-transition = +scrollSpeed+ 's';
+	//document.getElementById('picture.translate_y').style.transition = +scrollSpeed+ 's';
+
+	picture.style.marginTop.classList.add('translate_y');
 
 }
