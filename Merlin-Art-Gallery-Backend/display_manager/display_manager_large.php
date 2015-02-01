efd99262f889bc1d3157d163c1ad4c5bfc7edf85
lieuzhenghong <lieuzhenghong@gmail.com>
2015-02-01 12:14:19 +0800

    OOPS

--- Merlin-Art-Gallery-Backend/display_manager/display_manager_large.php
+++ Merlin-Art-Gallery-Backend/display_manager/display_manager_large.php
@@ -6,7 +6,6 @@
 	<link rel = "stylesheet" type = "text/css" href = "./css/style_large.css">
 	<link rel = "stylesheet" type = "text/css" href = "./css/form_style.css">
 	<link rel = "stylesheet" type = "text/css" href = "./css/table_style.css">
-	<link rel = "stylesheet" type = "text/css" href = "./bootstrap-3.3.2-dist/css/bootstrap.css">
 
 	<link href='http://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>
     <?php
