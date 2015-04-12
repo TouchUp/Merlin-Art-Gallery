ce7cc127b624b121121ddd5d3181fbd2f1367b30
ddjj1004 <ddjj1004@hotmail.com>
2015-04-12 21:17:31 +0800

    database update

--- imageserver.sql
+++ imageserver.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 22, 2015 at 07:08 AM
+-- Generation Time: Apr 12, 2015 at 03:16 PM
 -- Server version: 5.6.21
 -- PHP Version: 5.6.3
 
@@ -47,18 +47,19 @@ CREATE TABLE IF NOT EXISTS `images` (
   `country` varchar(20) DEFAULT NULL,
   `media` smallint(5) DEFAULT '1',
   `subject` smallint(5) DEFAULT '1'
-) ENGINE=InnoDB AUTO_INCREMENT=183 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=146 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `images`
 --
 
 INSERT INTO `images` (`pkey`, `code`, `name`, `artist`, `price`, `height`, `width`, `bio`, `sold`, `others`, `doby`, `dobm`, `dobd`, `plocation`, `pyear`, `country`, `media`, `subject`) VALUES
-(140, '51', 'lolwut', 'artist1', '99.00', 50.0100, 9.0000, '', 0, 'kek', 0, 0, 1, '', 2005, 'Singapore', 2, 2),
-(141, '50', 'The Last Supper', 'Leonardo da Vinci', '99.00', 54.0000, 1.0000, '', 0, '', 9, 0, 0, 'singapore', 0, '', 3, 1),
-(142, 'a24d', 'Mona Lisa', 'Leonardo da Vinci', '2.00', 0.0000, 40.0100, '', 0, '', 0, 0, 0, '', 0, '', 3, 4),
-(143, '3', 'Guernica', 'Pablo Picasso', '10.00', 0.0000, 0.0000, '', 0, '', 0, 0, 0, 'Singapore', 0, '', 1, 3),
-(144, '4', 'Girl with a Pearl Earring', 'Johannes Vermeer', '99.00', 0.0000, 0.0000, '', 0, '', 0, 0, 0, '', 0, '', 1, 3);
+(140, '51', 'lolwut', 'artist1', '99.00', 5.0000, 5.0000, 'None', 0, 'kek', 1994, 0, 1, '', 2005, 'Singapore', 2, 2),
+(141, '50', 'The Last Supper', 'Leonardo da Vinci', '99.00', 460.0000, 880.0000, 'None', 0, '', 1452, 0, 0, 'singapore', 0, '', 1, 1),
+(142, 'a24d', 'Mona Lisa', 'Leonardo da Vinci', '2.00', 76.2000, 53.3400, 'The Mona Lisa (Monna Lisa or La Gioconda in Italian; La Joconde in French) is a half-length portrait of a woman by the Italian artist Leonardo da Vinci, which has been acclaimed as "the best known, the most visited, the most written about, the most sung about, the most parodied work of art in the world".', 2, '', 0, 0, 0, '', 0, '', 2, 1),
+(143, '3', 'Guernica', 'Pablo Picasso', '10.00', 349.0000, 776.0000, 'None', 2, '', 0, 0, 0, 'Singapore', 0, '', 1, 3),
+(144, '4', 'Girl with a Pearl Earring', 'Johannes Vermeer', '99.00', 44.5000, 39.0000, 'None', 0, '', 0, 0, 0, '', 0, '', 1, 3),
+(145, '53', 'A4 Paper', 'Dae Koon', '1.00', 21.0000, 29.7000, 'None', 0, 'None', 1996, 7, 12, 'Singapore', 2015, 'Singapore', 3, 1);
 
 -- --------------------------------------------------------
 
@@ -69,7 +70,7 @@ INSERT INTO `images` (`pkey`, `code`, `name`, `artist`, `price`, `height`, `widt
 CREATE TABLE IF NOT EXISTS `mediaid` (
 `pkey` smallint(5) NOT NULL,
   `media` varchar(15) NOT NULL DEFAULT ''
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `mediaid`
@@ -100,7 +101,7 @@ INSERT INTO `subjectid` (`pkey`, `subject`) VALUES
 (1, 'scenery'),
 (2, 'person'),
 (3, 'random'),
-(8, 'object');
+(8, 'objects');
 
 --
 -- Indexes for dumped tables
@@ -132,12 +133,12 @@ ALTER TABLE `subjectid`
 -- AUTO_INCREMENT for table `images`
 --
 ALTER TABLE `images`
-MODIFY `pkey` int(8) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=183;
+MODIFY `pkey` int(8) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=146;
 --
 -- AUTO_INCREMENT for table `mediaid`
 --
 ALTER TABLE `mediaid`
-MODIFY `pkey` smallint(5) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+MODIFY `pkey` smallint(5) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT for table `subjectid`
 --
